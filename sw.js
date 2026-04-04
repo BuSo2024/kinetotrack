f8476c24f0199dd89529d1a407e3da9ebc2ff71b
sorin <sorin.busan@gmail.com>
2026-04-04 22:05:15 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 const CACHE_NAME = "kinetotrack-v1";
 
 const urlsToCache = [
-  "/kinetotrack/",
-  "/kinetotrack/index.html",
-  "/kinetotrack/manifest.json",
-  "/kinetotrack/icons/icon-192.png",
-  "/kinetotrack/icons/icon-512.png"
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
 ];
 
 self.addEventListener("install", event => {
